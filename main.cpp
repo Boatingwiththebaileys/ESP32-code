35fc084894b093500118a28c9505523a4aafc63f
BoatingWithTheBaileys <97848936+Boatingwiththebaileys@users.noreply.github.com>
2023-02-26 19:03:46 +0000

    Update main.cpp
    Update temp config and added smoothing to temp sensor and RPM - Code working well.

--- main.cpp
+++ main.cpp
@@ -35,15 +35,16 @@ class TemperatureInterpreter : public CurveInterpolator {
     // our temperature sender to degrees Kelvin
     clear_samples();
     // addSample(CurveInterpolator::Sample(knownOhmValue, knownKelvin));
-    add_sample(CurveInterpolator::Sample(40, 393.15));
-    add_sample(CurveInterpolator::Sample(70, 383.15));
-    add_sample(CurveInterpolator::Sample(80, 373.15));
-    add_sample(CurveInterpolator::Sample(92, 363.15));
-    add_sample(CurveInterpolator::Sample(110, 353.15));
-    add_sample(CurveInterpolator::Sample(140, 343.15));
-    add_sample(CurveInterpolator::Sample(165, 333.15));
-    add_sample(CurveInterpolator::Sample(210, 323.15));
-    add_sample(CurveInterpolator::Sample(300, 313.15)); 
+    add_sample(CurveInterpolator::Sample(20, 393.15));
+    add_sample(CurveInterpolator::Sample(30, 383.15));
+    add_sample(CurveInterpolator::Sample(40, 373.15));
+    add_sample(CurveInterpolator::Sample(55, 363.15));
+    add_sample(CurveInterpolator::Sample(70, 353.15));
+    add_sample(CurveInterpolator::Sample(100, 343.15));
+    add_sample(CurveInterpolator::Sample(140, 333.15));
+    add_sample(CurveInterpolator::Sample(200, 323.15));
+    add_sample(CurveInterpolator::Sample(300, 317.15));
+    add_sample(CurveInterpolator::Sample(400, 313.15)); 
   }
 };
 
@@ -101,11 +102,9 @@ void setup() {
 
   auto* exhaust_temp =
       new OneWireTemperature(dts, 1000, "/Exhaust Temperature/oneWire");
-
+ 
   exhaust_temp->connect_to(new Linear(1.0, 0.0, "/Exhaust Temperature/linear"))
-      ->connect_to(
-          new SKOutputFloat("propulsion.main.exhaustTemperature",
-                             "/Exhaust Temperature/sk_path"));
+      ->connect_to(new SKOutputFloat("propulsion.main.exhaustTemperature","/Exhaust Temperature/sk_path"));
 
 
  //RPM Application/////
@@ -118,7 +117,7 @@ void setup() {
 
   sensor->connect_to(new Frequency(multiplier, config_path_calibrate))  
   // connect the output of sensor to the input of Frequency()
-
+          >connect_to(new MovingAverage(2, 1.0,"/Engine RPM/movingAVG"))
          ->connect_to(new SKOutputFloat("propulsion.main.revolutions", config_path_skpath));  
           // connect the output of Frequency() to a Signal K Output as a number
 
@@ -146,38 +145,40 @@ void setup() {
 
   // Send the temperature to the Signal K server as a Float
   engine_room_temp->connect_to(new SKOutputFloat("propulsion.engineRoom.temperature"));
-
   engine_room_pressure->connect_to(new SKOutputFloat("propulsion.engineRoom.pressure"));
 
 //// Engine Temp Config ////
 
 const float Vin = 3.3;
-const float R1 = 1000.0;
+const float R1 = 120.0;
 auto* analog_input = new AnalogInput(36, 2000);
 
-analog_input->connect_to(new AnalogVoltage())
+analog_input->connect_to(new AnalogVoltage(Vin, Vin))
       ->connect_to(new VoltageDividerR2(R1, Vin, "/Engine Temp/sender"))
       ->connect_to(new TemperatureInterpreter("/Engine Temp/curve"))
-      ->connect_to(new Linear(1.0, 0.0, "/Engine Temp/calibrate"))
+      ->connect_to(new Linear(1.0, 0.9, "/Engine Temp/calibrate"))
+      ->connect_to(new MovingAverage(4, 1.0,"/Engine Temp/movingAVG"))
       ->connect_to(new SKOutputFloat("propulsion.engine.temperature", "/Engine Temp/sk_path"));
  
  //// Bilge Monitor /////
 
 auto* bilge = new DigitalInputState(25, INPUT_PULLUP, 5000);
 
-auto int_to_bool_function = [](int input) ->bool {
+auto int_to_string_function = [](int input) ->String {
      if (input == 1) {
-       return true;
+       return "Water present!";
      } 
      else { // input == 0
-       return false;
+       return "bilge clear";
      }
 };
 
-auto int_to_bool_transform = new LambdaTransform<int, bool>(int_to_bool_function);
+auto int_to_string_transform = new LambdaTransform<int, String>(int_to_string_function);
 
-bilge->connect_to(int_to_bool_transform)
-      ->connect_to(new SKOutputBool("propulsion.engine.bilge"));
+bilge->connect_to(int_to_string_transform)
+      ->connect_to(new SKOutputString("propulsion.engine.bilge"));
+
+bilge->connect_to(new SKOutputString("propulsion.engine.bilge.raw"));
 
 
   // Start networking, SK server connections and other SensESP internals
